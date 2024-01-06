4fffd90997ab387bb2cfa3d76ac36eb7d01c5405
ruchitakb7 <138369727+ruchitakb7@users.noreply.github.com>
2024-01-06 15:08:29 +0530

    Create database.js file

---
+++ util/database.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2')
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database:'node-complete',
+    password:'ruchita123@'
+})
+module.exports=pool.promise();
